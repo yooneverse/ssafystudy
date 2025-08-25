e77f9080fa3c351c7a7e4edb4df7bec264585252
donghwi <1116ehdgnl@naver.com>
2025-08-25 22:26:12 +0900

    0825_BAEK_2839_제출

--- donghwi/week4/BAEK_2839_설탕배달.py
+++ donghwi/week4/BAEK_2839_설탕배달.py
@@ -2,7 +2,7 @@ def sugar(N):
     result = -1
     if 0 == N % 5:
         return N // 5
-    elif 0 < N // 5:
+    elif 0 <= N // 5:
         for i in range((N // 5) + 1):
 
             if 0 == (N - i * 5) % 3:
