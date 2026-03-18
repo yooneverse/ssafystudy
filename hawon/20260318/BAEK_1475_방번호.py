a12230a0bfc65f4dfbfd835de3ff44bafc786dc0
조하원 <godhw1018@naver.com>
2026-03-18 20:07:13 +0900

    BAEK_1475_방번호_하원

--- hawon/20260318/BAEK_1475_방번호.py
+++ hawon/20260318/BAEK_1475_방번호.py
@@ -2,19 +2,3 @@
 BAEK_1475_방번호
 쉬움
 '''
-
-n = input()
-
-count = [0] * 10
-
-for ch in n:
-    num = int(ch)
-    count[num] += 1
-
-# 6과 9는 서로 바꿔 쓸 수 있으므로 합쳐서 계산
-six_nine = count[6] + count[9]
-
-count[6] = (six_nine + 1) // 2
-count[9] = count[6]
-
-print(max(count))
